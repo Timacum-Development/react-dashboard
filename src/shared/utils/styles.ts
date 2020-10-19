fa13a0a8a80a0c30840214f7e214165870ce2f19
milan-timacum <milan@timacum.com>
2020-10-19 09:50:32 +0200

    Basic

--- src/shared/utils/styles.ts
+++ src/shared/utils/styles.ts
@@ -1,19 +1,8 @@
 export const color = {
 	white: '#FFFFFF',
-	black: '#000000',
-	base: '#202124', // black
-	primary: '#0040BF', // blue
-	success: '#008000', // green
-	danger: '#BF0C12', // red
-	warning: '#E67E22', // orange
-	secondary: '#DADCE0', // light grey
-	background: '#FAFAFA', //bg light color
-	placeholder: 'rgba(31, 32, 35, 0.6)',
 };
 
 export const font = {
 	regular:
 		'font-family: "Roboto", "Helvetica", "Arial", sans-serif font-weight: normal',
-	medium:
-		'font-family: "Roboto", "Helvetica", "Arial", sans-serif font-weight: 500',
 };
